0d93c9042fff18704c5cc4c7882681ba0a91624a
Hayden Wilson <Hayden@SAEON.ac.za>
2019-05-27 11:02:24 +0200

    Delete Iterate_add_area.py

--- Iterate_add_area.py
+++
@@ -1,6 +0,0 @@
-import arcpy
-env.workspace = "E:/Hayden Working/test"
-fcList = arcpy.ListFeatureClasses()
-for fc in fcList:
-    arcpy.AddField_management(fc, "Area_ha", "DOUBLE")
-    arcpy.CalculateField_management(fc, "Area_ha", "!shape.area@hectares!", "PYTHON");
